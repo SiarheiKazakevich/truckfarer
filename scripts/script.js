91e77d6296ae8bb4917c80e1b477fb65448de90d
Siarhei Kazakevich <dianrom@tut.by>
2026-03-27 05:10:47 +0300

    feat: add interactive for burger

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,8 @@
 function toggleMenu() {
-  //document.getElementById('menu').classList.toggle('active');
+  const menu = document.getElementById('menu');
+  const burger = document.getElementById('burger')
+  menu.classList.toggle('active');
+  burger.classList.toggle('active');
 }
 
 function go(n) {
